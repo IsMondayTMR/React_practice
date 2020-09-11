7244e49fe35e2565af8c551455b8aa5fb1c10eca
IsMondayTMR <47375192+IsMondayTMR@users.noreply.github.com>
2020-09-11 14:58:22 -0700

    React template

---
+++ my-app/src/components/Myinfo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../css/Myinfo.css';
+function MyInfo(){
+    return (
+        <div>
+            <h1>Bo An</h1>
+            <p>This is a short paragraph about me</p>
+            <ul>
+                <li>test1</li>
+                <li>test2</li>
+                <li>test3</li>
+
+                </ul>
+        </div>
+    )
+}
+
+export  default MyInfo
